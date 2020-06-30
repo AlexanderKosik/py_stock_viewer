ce321ba46e267b6ce63d707183ef9978f3c1263e
Alexander Kosik <alexander.kosik@aareal-bank.com>
2020-06-30 18:08:22 +0200

    Add bar chart with dividends

--- gui.py
+++ gui.py
@@ -34,20 +34,40 @@ class MainGUI:
         self.txt_avg_long = Spinbox(master=self.root, from_=10.0, to=1000, increment=5)
         self.txt_avg_long.set(50)
         self.txt_avg_long.grid(column=1, row=2)
+        self.show_divs = IntVar()
+        self.cb_show_divs = Checkbutton(master=self.root, text="Show Dividends", var=self.show_divs, onvalue = 1, offvalue = 0)
+        self.cb_show_divs.grid(column=1, row=3, sticky=W)
 
         # Add a update button
-        self.btn_update = Button(master=self.root, text="Update", command=self._update_stock_chart).grid(column=1, row=3, sticky=W+E)
+        self.btn_update = Button(master=self.root, text="Update", command=self._update_stock_chart).grid(column=1, row=4, sticky=W+E)
 
         self._update_stock_chart()
 
     def _create_stock_chart(self):
-        figure = Figure(figsize=(13, 5), dpi=100)
-        a = figure.add_subplot()
+        # query data
         stock_config = stock_data.StockConfig(self.txt_stock.get(), int(self.txt_avg_short.get()), int(self.txt_avg_long.get()))
         data = stock_data.StockData(stock_config)
-        ax = sns.lineplot(hue="Events", dashes=False, markers=True, data=data.closing, label="Closing price", ax=a)
-        ay = sns.lineplot(hue="Events", dashes=False, markers=True, data=data.avg_short, label="Rolling avg short", ax=a)
-        az = sns.lineplot(hue="Events", dashes=False, markers=True, data=data.avg_long, label="Rolling avg long", ax=a)
+
+        # check if dividend data is available
+        dividend_greater_zero = data.data['Dividends'] > 0.0
+        divs = data.data[dividend_greater_zero]
+
+        figure = Figure(figsize=(16, 8), dpi=100)
+        # check if we have divs and should draw divs
+        if self.show_divs.get() == 1 and divs.empty is False:
+            stock_plot = figure.add_subplot(121)
+            dividend_plot = figure.add_subplot(122)
+            divs['Dividends'].index = divs['Dividends'].index.strftime("%Y")
+            if all(divs['Dividends'].values):
+                sns.barplot(x=divs['Dividends'].index, y=divs['Dividends'].values, palette="rocket", ax=dividend_plot)
+        else:
+            # draw only single chart
+            stock_plot = figure.add_subplot(111)
+
+        ax = sns.lineplot(hue="Events", dashes=False, markers=True, data=data.closing, label="Closing price", ax=stock_plot)
+        ay = sns.lineplot(hue="Events", dashes=False, markers=True, data=data.avg_short, label="Rolling avg short", ax=stock_plot)
+        az = sns.lineplot(hue="Events", dashes=False, markers=True, data=data.avg_long, label="Rolling avg long", ax=stock_plot)
+
         return figure
 
     def _update_stock_chart(self, *_): # *_ to be used with events
--- stock_data.py
+++ stock_data.py
@@ -9,9 +9,9 @@ class StockConfig:
 
 class StockData:
     def __init__(self, config: StockConfig):
-        self._data = yf.download(config.stock_name, period="3y", interval="1d")
+        self.data = yf.download(config.stock_name, period="3y", interval="1d", actions=True)
         # data = yf.download(ticker, period="6mo", interval="1d")
 
-        self.closing = self._data.get("Close")
+        self.closing = self.data.get("Close")
         self.avg_short = self.closing.rolling(window=config.avg_short).mean()
         self.avg_long = self.closing.rolling(window=config.avg_long).mean()
